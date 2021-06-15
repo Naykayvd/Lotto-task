8b276e7eac71534fcf543a3409c762f2b2b2990e
NayKayvd <vandiemennahum@gmail.com>
2021-06-15 14:52:35 +0200

    updated

--- lotto-login.py
+++ lotto-login.py
@@ -1,6 +1,4 @@
-import datetime
 import tkinter as tk
-import rsaidnumber
 from tkinter import *
 from tkinter import messagebox
 from datetime import *
@@ -17,11 +15,30 @@ def erase():
     email_entry.delete(0, "end")
     ID_entry.delete(0, "end")
 
+
 def check():
-    user_id = ID_entry
+    user_id = ID_entry.get()
     year = user_id[:2]
     if year >= "22":
         year = "19" + year
+    else:
+        year = "20" + year
+    month = user_id[2:4]
+    day = user_id[4:6]
+    today = date.today()
+
+    age = today.year - int(year) - ((today.month, today.day) < (int(month), int(day)))
+
+    if len(user_id) != 13:
+        messagebox.showerror("Error", "Please enter valid ID number")
+    elif age >= 18:
+        messagebox.showinfo("Welcome", "Let's Play")
+        window.destroy()
+        import lotto_game
+    else:
+        age = 18 - age
+        messagebox.showerror("Warning", "You are too young to play. Please try again in " + str(age) + " years")
+
 
 name_label = Label(window, text="Name:")
 name_label.place(x=100, y=50)
@@ -40,7 +57,7 @@ name = name_entry.get()
 email = email_entry.get()
 ID = ID_entry.get()
 
-enter_btn = Button(window, text="Validate")
+enter_btn = Button(window, text="Validate", command=check)
 enter_btn.place(x=50, y=250)
 clear_btn = Button(window, text="Clear", command=erase)
 clear_btn.place(x=200, y=250)
