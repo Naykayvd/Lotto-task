7534cca3166543f3edeb90696e13a2a2b8dcc4c2
NayKayvd <vandiemennahum@gmail.com>
2021-06-18 08:44:19 +0200

    updated project

--- lotto_game.py
+++ lotto_game.py
@@ -35,15 +35,22 @@ lotto_num5 = Spinbox(window, from_=1, to=49)
 lotto_num5.place(x=410, y=100, width=40, height=40)
 lotto_num6 = Spinbox(window, from_=1, to=49)
 lotto_num6.place(x=470, y=100, width=40, height=40)
+results = Label(window)
+results.place(x=90, y=160)
+total_winnings = Entry(window, state="readonly")
+total_winnings.place(x=25, y=405)
+
+lotto_prizes = {"zero": 0, "one": 0, "two": 20.00, "three": 100.50, "four": 2384.00, "five": 8584.00,
+                "six": 10000000.00}
 
 nums = []
+totals = []
 set01 = []
 set02 = []
 set03 = []
 set01.sort()
 set02.sort()
 set03.sort()
-result = set(set01), (set02), (set02).intersection(nums)
 
 
 def generate():
@@ -57,6 +64,21 @@ def generate():
         lotto_nums = [int(lotto_num1.get()), int(lotto_num2.get()), int(lotto_num3.get()), int(lotto_num4.get()),
                       int(lotto_num5.get()), int(lotto_num6.get())]
         print(lotto_nums)
+        matched_list = list(set(nums).intersection(lotto_nums))
+        print(matched_list)
+        matched_numbers = len(matched_list)
+        print(matched_numbers)
+
+        global counter
+        if counter == 1:
+            # game 1 use set01
+            counter += 1
+        elif counter == 2:
+            # game 2 use set02
+            counter += 1
+        elif counter == 3:
+            # game 3 use set03
+            counter += 1
 
         for k in range(len(number)):
             if len(set01) < 6:
@@ -65,21 +87,15 @@ def generate():
                 set02.append(number[k])
             elif len(set02) == 6 and len(set03) < 6:
                 set03.append(number[k])
-    except:
-        pass
-
-
-def play():
-    global counter
-    if counter == 1:
-        # game 1 use set01
-        counter += 1
-    elif counter == 2:
-        # game 2 use set02
-        counter += 1
-    elif counter == 3:
-        # game 3 use set03
-        counter += 1
+    finally:
+        if counter > 3:
+            messagebox.showinfo("Complete", "You have used all 3 of your turns")
+        else:
+            messagebox.showinfo("Finished", "Round ended")
+        results.config(text="You have " + str(matched_numbers) + " match(es) and you won R" + str(list(lotto_prizes.values())[matched_numbers]) +
+                       "\n" + "Your numbers were: " + str(lotto_nums) + "\n" +
+                       "And the winning numbers are: " + str(nums) + "\n" +
+                       str(date))
 
 
 def remove():
@@ -110,5 +126,4 @@ clear_button.place(x=540, y=450)
 exit_button = Button(window, text="EXIT", command=exit)
 exit_button.place(x=620, y=450)
 
-generate()
 window.mainloop()
